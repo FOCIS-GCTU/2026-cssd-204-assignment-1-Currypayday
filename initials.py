5067802ad5965c18b057747671fec3b8e30e0560
currypayday <143908000+Currypayday@users.noreply.github.com>
2026-05-31 17:44:16 +0000

    Add files via upload
    Scripting Assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,31 @@
+# File: initials.py
+# Description: A python program that prints the initials of my name "ACD"
+# Assignment Number: 2
+#
+# Name: COURAGE DESMOND AGBO
+# STUDENT ID: 2425401665
+# Email: 2425401665@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Courage Desmond Agbo, this programming assignment is my own work
+# and I have not provided this code to any other student.
+ 
 
 def main():
-  pass #code goes here
-
-
-
+    #This is the main function of my program
+    print()
+    print("...ACD")
+    print()
+    print(".....AAAAAAAA............CCCCCCCC..........DDDDDDDDDD.......")
+    print("....AAA....AAA..........CCCCCCCCCC.........DDDDDDDDDDD......")
+    print("....AAA....AAA.........CCCC................DDDD....DDDD.....")
+    print("...AAAA....AAAA........CCCC................DDDD....DDDD.....")
+    print("...AAAAAAAAAAAA........CCCC................DDDD....DDDD.....")
+    print("...AAAA....AAAA........CCCC................DDDD....DDDD.....")
+    print("...AAAA....AAAA........CCCC................DDDD....DDDD.....")
+    print("...AAAA....AAAA........CCCC................DDDD....DDDD.....")
+    print("...AAAA....AAAA...**....CCCCCCCCCC....**...DDDDDDDDDDD....**")
+    print("...AAAA....AAAA...**.....CCCCCCCC.....**...DDDDDDDDDD.....**")
+    print()
+    
 main()
